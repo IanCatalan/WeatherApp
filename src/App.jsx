c4fb845dc0ee21c34300806fedeb66a1c1aedd01
IanCat <iancatalan07@gmail.com>
2024-11-28 11:49:54 -0300

    second

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
     <div className="app">
         {
             (weather == undefined)
-            ?<Loading/>
+            ?<Loading hasError={hasError}/>
             :(
                 <WeatherCard 
                 weather={weather} 
